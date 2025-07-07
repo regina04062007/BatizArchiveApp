117e39a13437f66b6c255caa0bbed7245c01438b
ReginaHerrera <160207579+regina04062007@users.noreply.github.com>
2025-07-06 23:55:21 -0600

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,76 +1,112 @@
-import React from 'react';  
-import { Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; 
+import React, { useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View, Alert, KeyboardAvoidingView, Platform, Image, ScrollView
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 import styles from './LoginStyles';
-import { useNavigation } from '@react-navigation/native'; 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 export default function Login() {
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
+  const [correo, setCorreo] = useState('');
+  const [contraseña, setContraseña] = useState('');
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  const handleLogin = async () => {
+
+    const correoRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+    
+    const contraseñaRegex = /^[a-zA-Z0-9#$%&]+$/;
+  
+    if (!correoRegex.test(correo)) {
+      Alert.alert('Correo inválido', 'El correo debe terminar en @gmail.com');
+      return;
+    }
+  
+    if (!contraseñaRegex.test(contraseña)) {
+      Alert.alert('Contraseña inválida');
+      return;
+    }
+  
+    try {
+      const response = await axios.post('http://192.168.0.9:5000/api/login', {
+        correo_usuario: correo,
+        contrasena_usuario: contraseña,
+      });
+  
+      const { token } = response.data;
+  
+      await AsyncStorage.setItem('token', token);
+      Alert.alert('Éxito', 'Inicio de sesión exitoso');
+      navigation.navigate('Inicio');
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+      Alert.alert('Error', error.response ? error.response.data.error : 'Error desconocido');
+    }
+  };
+  
 
   return (
     <KeyboardAvoidingView
       style={styles.container}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-         <TouchableOpacity
-                style={styles.botonAtras}
-                onPress={() => navigation.goBack()}
-              >
-                <Icon name="arrow-left" size={30} color="black" />
-              </TouchableOpacity>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
         <View style={styles.header}>
           <Text style={styles.headerText}>INICIO DE SESIÓN</Text>
         </View>
 
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Correo Electrónico"
-            style={styles.input}
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Icon
-            name="email"
-            size={28}
-            color="#0EA18E"
-            style={styles.icon}
-          />
-        </View>
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput placeholder="Correo Electrónico" maxLength={50} style={styles.input} keyboardType="email-address" value={correo} onChangeText={setCorreo} autoCapitalize="none" placeholderTextColor="#7E21A3" />
+            <Icon name="email-outline" size={24} color="#7E21A3" style={styles.icon} />
+          </View>
 
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Contraseña"
-            secureTextEntry={true}  
-            style={styles.input}
-          />
-          <Icon
-            name="eye"
-            size={28}
-            color="#0EA18E"
-            style={styles.icon}
-          />
-        </View>
+          <View style={styles.inputContainer}>
+            <TextInput placeholder="Contraseña" maxLength={20} style={styles.input} value={contraseña} onChangeText={setContraseña}secureTextEntry={true} placeholderTextColor="#7E21A3" />
+            <Icon name="lock-outline" size={24} color="#7E21A3" style={styles.icon} />
+          </View>
 
-        <View style={styles.linkContainer}>
-          <Text style={styles.link}>¿Olvidaste tu contraseña?</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('RecuperarContra')}> 
-              <Text style={{ color: '#0EA18E', fontSize: 15 }}> Recuperar contraseña</Text> 
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>INICIAR SESIÓN</Text>
           </TouchableOpacity>
-        </View>
 
-        <View style={styles.linkContainer2}>
-          <Text style={styles.link2}>
-            ¿Todavía no tienes cuenta? 
-            <TouchableOpacity onPress={() => navigation.navigate('Register')}> 
-              <Text style={{ color: '#0EA18E', fontSize: 15 }}> Regístrate</Text> 
+          <View style={styles.linkContainer2}>
+            <Text style={styles.link}>¿Todavía no tienes cuenta?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+              <Text style={styles.linkHighlight}>Regístrate</Text>
             </TouchableOpacity>
-          </Text>
-        </View>
+          </View>
 
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>INICIAR SESIÓN</Text>
-        </TouchableOpacity>
+
+          <View style={styles.linkContainer}>
+            <Text style={styles.link}>¿Olvidaste tu contraseña?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('RecuperarContra')}>
+              <Text style={styles.linkHighlight}>Recuperar contraseña</Text>
+            </TouchableOpacity>
+          </View>
+
+          
+        </View>
       </ScrollView>
     </KeyboardAvoidingView>
   );
