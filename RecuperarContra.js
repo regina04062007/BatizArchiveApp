2157d63b4b7d22c5d39b8f65ea0bd75f4cc181d5
ReginaHerrera <160207579+regina04062007@users.noreply.github.com>
2025-07-06 23:52:43 -0600

    Update RecuperarContra.js

--- RecuperarContra.js
+++ RecuperarContra.js
@@ -7,48 +7,51 @@ import { useNavigation } from '@react-navigation/native';
 export default function RecuperarContra() {
   const navigation = useNavigation(); 
 
-    return (
-      <KeyboardAvoidingView
-        style={styles.container}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+    >
+<ScrollView contentContainerStyle={{ flexGrow: 1 }}keyboardShouldPersistTaps="handled">
+  <View style={styles.fullContent}>
+    <View style={styles.header}>
+      <Text style={styles.headerText}>RECUPERAR CONTRASEÑA</Text>
+    </View>
+
+    <View style={styles.footer}>
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Correo Electrónico"
+        style={styles.input}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        placeholderTextColor="#7E21A3"
+      />
+      <Icon
+        name="email-outline"
+        size={24}
+        color="#7E21A3"
+        style={styles.icon}
+      />
+    </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('CambiarContra')}
+        style={styles.button}
       >
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-           <TouchableOpacity
-                  style={styles.botonAtras}
-                  onPress={() => navigation.goBack()}
-                >
-                  <Icon name="arrow-left" size={30} color="black" />
-                </TouchableOpacity>
-          <View style={styles.header}>
-            <Text style={styles.headerText}>RECUPERAR CONTRASEÑA</Text>
-          </View>
+        <Text style={styles.buttonText}>SIGUIENTE</Text>
+      </TouchableOpacity>
 
-          <View style={styles.linkContainer2}>
-            <Text style={styles.link2}>
-              Use su correo Electrónico 
-            </Text>
-          </View>
+      <View style={styles.linkContainer}>
+        <Text style={styles.link}>¿Recordaste tu contraseña?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.linkHighlight}>Iniciar sesión</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  </View>
+</ScrollView>
 
-
-          <View style={styles.inputContainer}>
-            <TextInput
-              placeholder="Correo Electrónico"
-              style={styles.input}
-              keyboardType="email-address"
-              autoCapitalize="none"
-            />
-            <Icon
-              name="email"
-              size={28}
-              color="#0C5D61"
-              style={styles.icon}
-            />
-          </View>
-
-          <TouchableOpacity onPress={() => navigation.navigate('CambiarContra')} style={styles.button}>
-            <Text style={styles.buttonText}>SIGUIENTE</Text>
-          </TouchableOpacity>
-        </ScrollView>
-      </KeyboardAvoidingView>
-    );
+    </KeyboardAvoidingView>
+  );
 }
+
