af921bb634c8609dce8020677316899e7439a2be
ReginaHerrera <160207579+regina04062007@users.noreply.github.com>
2025-07-06 23:53:40 -0600

    Update Principal.js

--- Principal.js
+++ Principal.js
@@ -1,49 +1,57 @@
-import React from 'react';  
-import { Text, TouchableOpacity, View, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; 
+import React from 'react';
+import { Text, TouchableOpacity, View, KeyboardAvoidingView, Platform, Image,ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import styles from './PrincipalStyles';
-import { useNavigation } from '@react-navigation/native'; 
-import { Image } from 'react-native';
+
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 export default function Principal() {
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
 
-    return (
-      <KeyboardAvoidingView
-        style={styles.container}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // Evita que el teclado cubra los campos
-      >
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-          <View style={styles.header}>
-          </View>
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
 
-          <View style={styles.textContainer2}>
-            <Text style={styles.text2}>
-              Bienvenido a tu BatizArchive 
-            </Text>
-          </View>
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
-          <Image
-            style={styles.imagenLogo}
-            source={require('./assets/logoBatiz.jpeg')}
-          />
-          
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'} >
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}keyboardShouldPersistTaps="handled">
+      <View style={styles.topSection}>
+        <Image
+          style={styles.imagenLogo}
+          source={require('./assets/logoBatizArchive.png')}
+        />
+        <Text style={styles.title}>BatizArchive</Text>
+        <Text style={styles.subtitle}>Tu gestor de proyectos</Text>
+      </View>
 
-          <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.button}>
-            <Text style={styles.buttonText}>INICIO DE SESIÓN</Text>
-          </TouchableOpacity>
+      <View style={styles.footer}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Login')}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>INICIAR SESIÓN</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => navigation.navigate('Register')} style={styles.button2}>
-            <Text style={styles.buttonText2}>REGISTRARSE</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => navigation.navigate('Inicio')} style={styles.button2}>
-            <Text style={styles.buttonText2}>INICIO</Text>
-          </TouchableOpacity>
-
-        </ScrollView>
-        <View style={styles.footer}>
-          </View>
-      </KeyboardAvoidingView>
-    );
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Register')}
+          style={[styles.button, styles.registerButton]}
+        >
+          <Text style={styles.registerButtonText}>REGISTRARSE</Text>
+        </TouchableOpacity>
+      </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
