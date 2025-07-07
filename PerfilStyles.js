06d93a3dbab9f38b40f65f9b2944569dadc60b3d
ReginaHerrera <160207579+regina04062007@users.noreply.github.com>
2025-07-06 23:54:06 -0600

    Update PerfilStyles.js

--- PerfilStyles.js
+++ PerfilStyles.js
@@ -3,80 +3,130 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   contenedor: {
     flexGrow: 1,
-    alignItems: "center",
-    justifyContent: "center",
     padding: 20,
-    marginTop: 30,
-    backgroundColor: "white",
+    backgroundColor: "#F3F4F6",
   },
 
-  botonAtras: {
-    position: "absolute",
-    top: 20,
-    left: 10,
+  seccion: {
+    marginBottom: 30,
+    paddingBottom: 20,
   },
 
-  botonCerrarSesion: {
-    position: "absolute",
-    top: 25,
-    right: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#0404B4",
-    paddingVertical: 8,
-    paddingHorizontal: 15,
-    borderRadius: 15,
-  },
-
-  textoCerrarSesion: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-    marginLeft: 5, 
+  tituloSeccion: {
+    fontSize: 28,
+    color: "#1f1f1f",
+    alignSelf: 'center',
+    marginBottom: 20,
+    marginTop: 20,
+    fontFamily: 'Poppins_700Bold',
   },
 
   contenedorImagen: {
-    backgroundColor: "#f2f2f2",
+    borderRadius: 15,
+    alignItems: "center",
+    marginBottom: 25,
+    backgroundColor: "#ffffff",
     padding: 20,
-    borderRadius: 10,
-    marginTop: 60, 
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 8,
   },
 
   imagenPerfil: {
-    width: 200,
-    height: 200,
-    borderRadius: 50,
+    width: 150,
+    height: 150,
+    borderRadius: 75,
   },
 
   infoUsuario: {
     alignItems: "center",
-    marginTop: 20,
+    marginTop: 15,
+  },
+
+  textoInfo1: {
+    fontSize: 22,
+    marginBottom: 5,
+    color: "#222",
+    fontFamily: 'Poppins_700Bold',
+    textAlign: "center",
   },
 
   textoInfo: {
-    fontSize: 20,
-    fontWeight: "bold",
+    fontSize: 17,
     marginBottom: 5,
-  },
-
-  tituloFavoritos: {
-    fontSize: 22,
-    fontWeight: "bold",
-    marginTop: 20,
-    color: "#6babae",
+    color: "#555",
+    fontFamily: 'Poppins_400Regular',
+    textAlign: "center",
   },
 
   textoFavorito: {
-    fontSize: 16,
-    marginBottom: 5,
+    fontSize: 17,
+    marginBottom: 10,
+    color: "#444",
+    fontFamily: 'Poppins_700Bold',
   },
 
-  verProyecto: {
+  proyectoCard: {
+    backgroundColor: '#fff',
+    padding: 20,
+    marginVertical: 10,
+    borderRadius: 12,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+  },
+
+  Button: {
     marginTop: 10,
-    backgroundColor: "#E6E6E6",
+    paddingVertical: 6,
+    paddingHorizontal: 16,
+    backgroundColor: '#6A0DAD', 
+    borderRadius: 30,
+    alignSelf: 'flex-start',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  
+  buttonText: {
+    color: 'white',
+    fontSize: 14,
+    fontFamily: 'Poppins_700Bold',
+    paddingHorizontal: 8,
+  },
+  
+
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
     paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 20,
+    backgroundColor: "#9A20E0",
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+
+  footerButton: {
+    alignItems: "center",
+  },
+
+  footerText: {
+    color: "white",
+    fontSize: 12,
+    marginTop: 5,
+    fontFamily: 'Poppins_400Regular',
   },
 });
-
